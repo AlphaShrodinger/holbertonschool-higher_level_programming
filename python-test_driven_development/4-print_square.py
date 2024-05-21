283875a0eb6eb34af27ccfb346b580331ef0f092
jose <joseoquendovazquez@gmail.com>
2024-05-20 21:30:44 -0400

    added comments to task 3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""print_square"""
+
+
 def print_square(size):
+    """prints a square"""
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:

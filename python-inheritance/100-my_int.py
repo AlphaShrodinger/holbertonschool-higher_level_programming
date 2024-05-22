9eea08075bb8b62979330dc1ef9807ed3d593005
jose <joseoquendovazquez@gmail.com>
2024-05-22 16:05:20 -0400

    fixed reverse values

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -2,11 +2,11 @@
 """my int"""
 
 
-class MyInt (int):
-    def __ev__(self, other):
+class MyInt(int):
+    def __eq__(self, other):
         """Return True if self and other not equal, else false"""
-        return int(self) == other
-
-    def __pk__(self, other):
-        """Return True if self and other equal, else false"""
         return int(self) != other
+
+    def __ne__(self, other):
+        """Return True if self and other equal, else false"""
+        return int(self) == other

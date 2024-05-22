6568a7bd597e4d261bffff09aebd21725ae8f0c9
jose <joseoquendovazquez@gmail.com>
2024-05-22 13:17:19 -0400

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Same class"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true if isinstance"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False

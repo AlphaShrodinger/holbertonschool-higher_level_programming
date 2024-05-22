18bd1306aa3db9d3ca5a7d2b5d8bc2dbb1274c2c
jose <joseoquendovazquez@gmail.com>
2024-05-22 14:04:23 -0400

    added more comments

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -20,6 +20,7 @@ class Rectangle(BaseGeometry):
     """Writes a rectangle"""
 
     def __init__(self, width, height):
+        """Validates width and height"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width

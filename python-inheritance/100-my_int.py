065418906fe59121f1ba399fd8595b55505c645c
jose <joseoquendovazquez@gmail.com>
2024-05-22 15:59:45 -0400

    modified code error

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -5,8 +5,8 @@
 class MyInt (int):
     def __ev__(self, other):
         """Return True if self and other not equal, else false"""
-        return int(self) != other
+        return int(self) == other
 
     def __pk__(self, other):
         """Return True if self and other equal, else false"""
-        return int(self) == other
+        return int(self) != other

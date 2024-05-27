f3dcba9b97d217f422ec05d852620862873e3748
jose <joseoquendovazquez@gmail.com>
2024-05-27 15:56:12 -0400

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Student file'''
+
+
+class Student:
+    '''Student class'''
+
+    def __init__(self, first_name, last_name, age):
+        '''init method'''
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def to_json(self, attrs=None):
+        '''to json method'''
+        if attrs is None:
+            return self.__dict__
+        new_dictionary = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dictionary[key] = value
+        return new_dictionary

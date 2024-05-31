cbbfac18a1581c6e8cfb1f31b2e6c9b80c6e0775
jose <joseoquendovazquez@gmail.com>
2024-05-31 14:15:30 -0400

    editing error message

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -34,7 +34,10 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
                     "description": "A simple API built with http.server"}
             self.wfile.write(bytes(json.dumps(info), "utf-8"))
         else:
-            self.send_error(404, "Endpoint not found")
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes("404 Not Found", "utf-8"))
 
 
 def run():

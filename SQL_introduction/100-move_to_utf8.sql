789546f6dff2adc14176d31524350e67a71ef31c
jose <joseoquendovazquez@gmail.com>
2024-06-17 19:23:41 -0400

    rewriting code

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -9,7 +9,4 @@ ALTER TABLE first_table
     CONVERT TO CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table
-    MODIFY COLUMN name VARCHAR(255)
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+SHOW CREATE TABLE first_table;

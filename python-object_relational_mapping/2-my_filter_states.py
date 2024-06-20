16fafb8892e90febd08c3035b8f7b87ef50d63bf
jose <joseoquendovazquez@gmail.com>
2024-06-20 18:51:04 -0400

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''takes in an argument and displays all values in the states table'''
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(user=argv[1],
+                               passwd=argv[2],
+                               db=argv[3])
+    curs = database.cursor()
+    curs.execute("SELECT * FROM states WHERE\
+                  name LIKE BINARY '{}'\
+                  ORDER BY states.id ASC".format(argv[4]))
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
+    curs.close()
+    database.close()

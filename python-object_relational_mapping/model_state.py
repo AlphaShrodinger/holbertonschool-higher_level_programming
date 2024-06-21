16afdf368a6ece3c5e328a8b498d2cba41f9b6cc
jose <joseoquendovazquez@gmail.com>
2024-06-20 20:31:19 -0400

    modifying script

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,15 +2,29 @@
 '''contains the class definition of a State and an instance'''
 
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+DB_URI = 'mysql://username:password@localhost:3306/database_name'
+engine = create_engine(DB_URI)
+Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
 
 class State(Base):
-    def __init__(self):
-        pass
     __tablename__ = 'states'
-    id = Column(Integer, nullable=False, primary_key=True)
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+
+
+if __name__ == '__main__':
+    from state import State
+    Base.metadata.create_all(engine)
+
+    session = Session()
+    new_state = State(name='New State')
+    session.add(new_state)
+    session.commit()
+    session.close()

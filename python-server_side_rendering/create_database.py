47ddd316614cce82be2b1a52fbf923ade3f70443
jose <joseoquendovazquez@gmail.com>
2024-07-15 23:32:17 -0400

    modifying code task 4

--- python-server_side_rendering/create_database.py
+++ python-server_side_rendering/create_database.py
@@ -5,19 +5,19 @@ def create_database():
     conn = sqlite3.connect('products.db')
     cursor = conn.cursor()
     cursor.execute('''
-         CREATE TABLE IF NOT EXISTS Products (
-             id INTEGER PRIMARY KEY,
-             name TEXT NOT NULL,
-             category TEXT NOT NULL,
-             price REAL NOT NULL
-         )
-     ''')
+        CREATE TABLE IF NOT EXISTS Products (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            category TEXT NOT NULL,
+            price REAL NOT NULL
+        )
+    ''')
     cursor.execute('''
-         INSERT INTO Products (id, name, category, price)
-         VALUES
-         (1, 'Laptop', 'Electronics', 799.99),
-         (2, 'Coffee Mug', 'Home Goods', 15.99)
-     ''')
+        INSERT INTO Products (id, name, category, price)
+        VALUES
+        (1, 'Laptop', 'Electronics', 799.99),
+        (2, 'Coffee Mug', 'Home Goods', 15.99)
+    ''')
     conn.commit()
     conn.close()
 
--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -1,26 +1,71 @@
+from flask import Flask, render_template, request, json
+import csv
 import sqlite3
 
+app = Flask(__name__)
 
-def create_database():
-    conn = sqlite3.connect('products.db')
+
+def read_json(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def read_csv(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        data = [row for row in csv_reader]
+    return data
+
+
+def read_sqlite(db_path):
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute('''
-         CREATE TABLE IF NOT EXISTS Products (
-             id INTEGER PRIMARY KEY,
-             name TEXT NOT NULL,
-             category TEXT NOT NULL,
-             price REAL NOT NULL
-         )
-     ''')
-    cursor.execute('''
-         INSERT INTO Products (id, name, category, price)
-         VALUES
-         (1, 'Laptop', 'Electronics', 799.99),
-         (2, 'Coffee Mug', 'Home Goods', 15.99)
-     ''')
-    conn.commit()
+    cursor.execute('SELECT id, name, category, price FROM Products')
+    rows = cursor.fetchall()
     conn.close()
+    data = [{'id': row[0], 'name': row[1],
+             'category': row[2], 'price': row[3]} for row in rows]
+    return data
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    product_id = request.args.get('id')
+
+    if source == 'json':
+        products = read_json('products.json')
+    elif source == 'csv':
+        products = read_csv('products.csv')
+    elif source == 'sql':
+        products = read_sqlite('products.db')
+    else:
+        return render_template('product_display.html', error="Wrong source")
+
+    if product_id:
+        products = [product for product in products if str(
+            product['id']) == str(product_id)]
+        if not products:
+            return render_template('product_display.html', error="Product not found")
+
+    return render_template('product_display.html', products=products)
 
 
 if __name__ == '__main__':
-    create_database()
+    app.run(debug=True, port=5000)
